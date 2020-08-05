d7c45feb90f82600029e699e60aec30b58cb8e0a
Mateus Filipe <mateusfilipe557@gmail.com>
2020-08-05 15:54:55 -0300

    Big Update on the Moving Button
    So happy that its goig well

--- btnFunctions.js
+++ btnFunctions.js
@@ -1,33 +1,45 @@
 var score = 0;
-var btnWait = false;
-const btnConst = true;
+var btnTempo = 0;
 function btnMove(){
-	document.getElementById("clickBtn").style.left= Math.floor(Math.random() * 1087)+200+"px";
-	document.getElementById("clickBtn").style.top= Math.floor(Math.random() * 621)+"px";
+	var btnLeft = Math.floor(Math.random() * 1087)+200;
+	var btnTop = Math.floor(Math.random() * 621);
+	document.getElementById("clickBtn").style.left= btnLeft+"px";
+	document.getElementById("clickBtn").style.top= btnTop+"px";
 }
 function btnClickFunction(){
+	/*antes = Date.now();
+	duracao = (Date.now()-antes)/1000;*/
 	score = score+1;
-	btnMove();
 	document.getElementById("pts").innerHTML = "Score: " + score;
+	btnMove();
 
 }
 function btnStopFunction(){
 	score = 0;
-	btnWait = false;
 	document.getElementById("clickBtn").style.left= 15+"px";
 	document.getElementById("clickBtn").style.top= 105+"px";
 	document.getElementById("pts").innerHTML = "Score: " + score;
-}
-function btnStartFunction(){
-	btnWait = true;
-	btnMoveTime();
+	//document.getElementById("time").innerHTML = "Time: " + duracao;
 }
 
-function btnMoveTime(){
-	while(btnConst){
-		setTimeout(function(){btnMove()}, 500);
-		if(score = 5){
-			break;
-		}
+function btnStartFunction(){
+	if(document.getElementById("easy").checked){
+		btnTempo = 3000;
+	}else if(document.getElementById("medium").checked){
+		btnTempo = 2500;
+	}else if(document.getElementById("hard").checked){
+		btnTempo = 1500;
+	}else if(document.getElementById("impossible").checked){
+		btnTempo = 300;
+	}else{
+		btnTempo = 4000;
 	}
+	setInterval(function(){ btnMove(); }, btnTempo);
 }
+
+/*function btnWaitMove(){
+	//code before the pause
+	setTimeout(function(){
+	    btnMove();
+	}, 100);
+}*/
