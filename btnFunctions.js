82f5863e9487242eb3109f72fccceb45a8ba1069
Mateus Filipe <mateusfilipe557@gmail.com>
2020-09-04 14:46:15 -0300

    Adding a time function
    So, now we have a time function and the score is hidden until the time is over. Thank Vruno by the idea.

--- btnFunctions.js
+++ btnFunctions.js
@@ -1,15 +1,18 @@
 var score = 0;
 var btnTempo = 0;
-var intervalTime = 20;
+var intervalTime = 0;
+var timeCtrlHandle = null;
 var intervalCtrl = null;
+var timeCtrl = null;
 var started = false;
 document.getElementsByName("Sobre").disabled = true;
 /*sizeWidth = window.innerWidth;
 sizeHeight = window.innerHeight;*/
 
 function btnMove(){
-	btnStartFunction();
-	var btnLeft = Math.floor(Math.random() * 1087)+104;
+	clearInterval(intervalCtrl);
+	intervalCtrl = setInterval(function(){ btnMove(); }, btnTempo);
+	var btnLeft = Math.floor(Math.random() * 1207)+104;
 	var btnTop = Math.floor(Math.random() * 621);
 	if(started){
 		document.getElementById("clickBtn").style.left= btnLeft+"px";
@@ -24,6 +27,8 @@ function btnClickFunction(){
 	btnMove();
 }
 function btnStartFunction(){
+	intervalTime = 20;
+	timerFunction();
 	clearInterval(intervalCtrl);
 	started = true;
 	if(document.getElementById("easy").checked){
@@ -33,7 +38,7 @@ function btnStartFunction(){
 	}else if(document.getElementById("hard").checked){
 		btnTempo = 700;
 	}else if(document.getElementById("impossible").checked){
-		btnTempo = 300;
+		btnTempo = 400;
 	}else{
 		btnTempo = 4000;
 	}
@@ -41,27 +46,30 @@ function btnStartFunction(){
 }
 function btnStopFunction(){
 	started = false;
-	score = 0;
 	btnTempo = 0;
-	intervalTime = 0;
+	intervalTime = 20;
 	clearInterval(intervalCtrl);
-	document.getElementById("clickBtn").style.left= 15+"px";
-	document.getElementById("clickBtn").style.top= 110+"px";
+	clearTimeout(timeCtrlHandle);
+	document.getElementById("clickBtn").style.left= 655.5+"px";
+	document.getElementById("clickBtn").style.top= 310.5+"px";
 	document.getElementById("pts").innerHTML = "Score: " + score;
 	document.getElementById("time").innerHTML = "Time: " + intervalTime + "s";
 }
 
-/*
-function timerFunction(){
-	window.setTimeout(timerFunction, 1000);
-	while(intervalTime >= 0){
-		document.getElementById("time").innerHTML = "Time: " + intervalTime + "s";
-		intervalTime = intervalTime - 1;
-	}	
-}
 
-var intervalTime = null;
-	for (var i = 20; i > 0; i--) {
-		intervalTime = setInterval(function(){document.getElementById("time").innerHTML = "Time: " + score+i;}, 1000);
+function timerFunction(){
+	var myTimer = document.getElementById("time");
+	if(intervalTime < 10){
+		myTimer.innerHTML = "Time: 0" + intervalTime + "s";
+	}else{
+		myTimer.innerHTML = "Time: " + intervalTime + "s";
 	}
-*/
+
+	if(intervalTime <= 0){
+		document.getElementById("pts").style.visibility= "visible";
+		btnStopFunction();
+		return;
+	}
+	intervalTime = intervalTime - 1;
+	timeCtrlHandle = window.setTimeout(timerFunction, 1000);
+}
